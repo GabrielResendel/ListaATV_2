57582876265de79d94eda8d3491105d0d88bcc62
GabrielResendel <codegabriel7@gmail.com>
2024-06-26 19:06:09 -0300

    atv 2

---
+++ 2_MediaNota/MediaNota.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace MediaNota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Diga as notas e some sua media!!");
+            Console.Write("Digite a nota 1: ");
+            double nota1 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite a nota 1: ");
+            double nota2 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite a nota 1: ");
+            double nota3 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite a nota 1: ");
+            double nota4 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double res = (nota1 + nota2 + nota3 + nota4) / 4;
+
+         Console.WriteLine($"A media das notas são: {(res).ToString("F2", CultureInfo.InvariantCulture)}");   
+        }
+    }
+}
