c2f02a4bc07ae16c5c8618266d66c1c08a52e3d3
GabrielResendel <codegabriel7@gmail.com>
2024-06-26 19:39:56 -0300

    atv 4

---
+++ 4_TotalSegundos/TotalSegundos.cs
@@ -0,0 +1,29 @@
+﻿// 4 Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos. (1 minutos tem quantos segundos? 1 hora tem quantos minutos?)
+using System;
+
+namespace TotalSegundos
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Calcule o total de segundos dizendo horas, minutos e segundos ");
+            Console.Write("Horas: ");
+            int hora = int.Parse(Console.ReadLine());
+            int horaS = 3600 * hora;
+
+            Console.Write("Minutos: ");
+            int min = int.Parse(Console.ReadLine());
+            int minS = 60 * min;
+
+            Console.Write("Segundos: ");
+            int seg = int.Parse(Console.ReadLine());
+
+            int res = horaS + minS + seg;
+
+        Console.WriteLine($"O total de segundo de {hora} Horas {min} Minutos {seg} Segundos é : {res}");   
+    
+        }
+    }
+
+}
